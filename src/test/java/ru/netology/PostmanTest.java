5ad3acff007640d4d7af83166ca347b8c4bdec4d
Krsitina Klimenchenko <klimenchenko.kri.ch@gmail.com>
2022-01-26 19:41:39 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.netology;
+import static io.restassured.RestAssured.given;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void shouldPostData() {
+        given()
+                //предусловие
+                .baseUri("https://postman-echo.com")
+                .body("user id") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                //выполняемые действия
+                .when()
+                .post("/post")
+
+                // проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("user id"))
+        ;
+    }
+}
